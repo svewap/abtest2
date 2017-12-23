6716f0f40f71c68903b9171686ff7bb28959dba4
Sven Wappler <info@wappler.systems>
2017-12-24 00:09:25 +0100

    source code beautification

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,55 +1,55 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', array(
-    'tx_abtest2_b_id' => array(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', [
+    'tx_abtest2_b_id' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:pages.tx_abtest2_b_id',
-        'config' => array(
+        'config' => [
             'type' => 'group',
             'internal_type' => 'db',
             'allowed' => 'pages',
             'maxitems' => 1,
             'size' => 1
-        )
-    ),
-    'tx_abtest2_cookie_time' => array(
+        ]
+    ],
+    'tx_abtest2_cookie_time' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:pages.tx_abtest2_cookie_time',
-        'config' => array(
+        'config' => [
             'type' => 'select',
             'renderType' => 'selectSingle',
-            'items' => array(
+            'items' => [
                 ['LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:cookie_1_month', 2419200],
                 ['LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:cookie_1_week', 604800],
                 ['LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:cookie_1_day', 86400],
                 ['LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:cookie_12_day', 43200],
                 ['LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:cookie_1_hour', 3600],
                 ['LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:cookie_1_minute', 60]
-            )
-        )
-    ),
-    'tx_abtest2_header' => array(
+            ]
+        ]
+    ],
+    'tx_abtest2_header' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:pages.tx_abtest2_header',
-        'config' => array(
+        'config' => [
             'type' => 'text'
-        )
-    ),
-    'tx_abtest2_footer' => array(
+        ]
+    ],
+    'tx_abtest2_footer' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:pages.tx_abtest2_footer',
-        'config' => array(
+        'config' => [
             'type' => 'text'
-        )
-    ),
-    'tx_abtest2_counter' => array(
+        ]
+    ],
+    'tx_abtest2_counter' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:pages.tx_abtest2_counter',
-        'config' => array(
+        'config' => [
             'type' => 'input'
-        )
-    )
-));
+        ]
+    ]
+]);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages',
     '--palette--;LLL:EXT:abtest2/Resources/Private/Language/locallang_db.xlf:palette_title;tx_abtest2', '',
