7b1c84806539169724224b96f341ee5dd9d31995
Sven Wappler <info@wappler.systems>
2017-12-25 01:13:38 +0100

    clean up

--- ext_tables.php
+++ ext_tables.php
@@ -1,11 +1,3 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-    function()
-    {
-
-        //\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('abtest2', 'Configuration/TypoScript', 'AB Test');
-
-    }
-);
